791497015374daebc72f31bbb6b26c8b8e3a8c8b
Hair Parra <jair.parra@outlook.com>
2019-09-22 15:21:29 -0400

    Update Assignment1.py

--- scripts/Assignment1.py
+++ scripts/Assignment1.py
@@ -34,6 +34,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns # easier & prettier visualization 
 from tqdm import tqdm # to display progress bar
 from numpy import transpose as T # because it is a pain in the ass
+sns.set()
 
 # *****************************************************************************
 
@@ -146,7 +147,7 @@ plt.savefig('../figs/cancer_countplot.png')
 
 # Features pairplot
 sns.pairplot(cancer_df.drop('Class',axis=1), diag_kind='kde')
-plt.savegif('../figs/cancer_pariplot.png')
+plt.savefig('../figs/cancer_pariplot.png')
 
 # Since the all the features seem to lie in similar ranges, we will not
 # normalize them. 
@@ -179,224 +180,12 @@ y_cancer = cancer_df['Class']
 
 # *****************************************************************************
 
-### 3. Helper Functions ### 
+### 3. TESTING LOGISITC REGRESSION ### 
 
-def train_test_split(X,y, train_size=0.8, test_size=0.2, random_state=42):   
-    np.random.seed(random_state)
-    
-    print("Not implemented")
-
-# *****************************************************************************
-
-### 4. Implementing Logistic Regression ### 
+# import the class from the script
+from LogisticRegression import LogisticRegression
 
 
-class LogisticRegression(): 
-    
-    np.random.seed(42) 
-    
-    def __init__(self,X=np.array([[0]]), y=np.array([0]) ): 
-        """
-        X: (n x m) matrix of features with n observations 
-         and m features. (in np matrix format)
-        y: (n x 1) vector of targets (as Series)
-        
-        """     
-        if X[0,0] == 0 and y[0] == 0: 
-            print("Default initialization") 
-            
-        # Verify dimensions
-        if X.shape[0] != y.shape[0]: 
-            message = "Input dimensions don't match" 
-            message += "\n X is {} but y is {}".format(X.shape, y.shape)
-            raise ValueError(message) 
-            
-        self.n = X.shape[0]
-        self.m = X.shape[1] + 1 # Because of intercept term 
-        X_0 = np.ones(self.n) # intercept features 
-        self.X = np.c_[X_0,X] # concatenate 
-        self.w = np.random.rand(self.m, 1) # randomly initialize weights 
-        self.y = y 
-        
-        print("Initialized with dimensions\n X:({}) y:({})".format(self.X.shape, self.y.shape)) 
-        print("Number of features: m={}".format(self.m)) 
-        print("Number of observations: n={}".format(self.n)) 
-        print("Number of weights: len(w)={}".format(len(self.w)))
-        
-        
-    def sigmoid(self, z): 
-        return 1/(1+ np.exp(-z))
-    
-    def predict_probabilities(self, X_new): 
-        """
-        Returns a probablistic prediction using the model 
-        parameters. 
-        inputs: 
-            @ self
-            @ X_new : (n' x m) input vector in list or 
-                      numpy format. 
-        """
-        X_new = np.array(X_new)
-        input_shape = X_new.shape
-        print(input_shape)
-        
-        # If input is a vector 
-        if X_new.shape[0] == 1: 
-            # If input length doesn't match  
-            if (X_.shape[1] + 1) != self.m: 
-                message = "Input number of features doesn't match model number of parameters" 
-                message += "\nInput is has {} features but model has {} features".format(len(X_new), self.m - 1)
-                raise Exception(message)
-            else: 
-                print("HERE")
-                x = np.insert(X_new, 0, 1) # insert an extra one at the beginning
-                wTx = float(np.matmul(T(self.w), x)) 
-                sigm_wTx = self.sigmoid(wTx) 
-                print("sigm_wTx", sigm_wTx)
-                return [sigm_wTx] 
-                
-        # if input is a matrix of new examples
-        elif input_shape[0] > 1 and input_shape[1] > 1: 
-            print("matrix")
-#           # if number of attributes don't match 
-            if (X_new.shape[1] + 1) != self.m: 
-                message = "Input dimensions don't match" 
-                message += "\nInput matrix contains {} features, but the model has {} fitted features".format(self.m - 1)
-                raise Exception(message)
-            # right dimensions
-            else: 
-                pred_probs = np.zeros((X_new.shape[0],1)) # to store the probs
-                X_0 = np.ones((X_new.shape[0],1)) # n-dim vector of ones
-                X_new = np.c_[X_0,X_new] # concatenate
-                
-                # since X_new is a matrix, we have to loop 
-                # over each of its rows, which comes out as
-                # a column vector 
-                for i in range(len(X_new)): 
-                    x = X_new[i] # row = example
-                    wTx = float(np.matmul(T(self.w),x)) # w^Tx 
-                    sigm_wTx = self.sigmoid(wTx)
-                    pred_probs[i] = sigm_wTx
-                    
-                return pred_probs
-            
-            
-    def predict(self, X_new): 
-        """
-        Returns an array of predictions for the 
-        new input. 
-        """
-        # get predictions 
-        probs = self.predict_probabilities(X_new) 
-        
-        # Use decision boundary
-        return [1 if prob >= 0.5 else 0 for prob in probs]
-                
-    # loss function
-    def cross_entropy_loss(self, verbose=False): 
-        
-        total_loss = []
-        # for each datapoint
-        for i in range(self.n): 
-            x_i = self.X[i] 
-            y_i = self.y[i] 
-            wTx = float(np.matmul(T(self.w), x_i)) #w^Tx
-            sigm_wTx = self.sigmoid(wTx)
-            
-            if verbose:
-                print("wTx: " , wTx) 
-                print("sigm_wTx ", sigm_wTx)
-                print("log(sigm_wTx) ", math.log(sigm_wTx))
-            
-            if y_i == 1:
-                total_loss.append(-1*math.log(sigm_wTx + 0.0001)) 
-            else: 
-                total_loss.append(1*math.log(1-sigm_wTx + 0.0001))
-            
-            
-        loss = np.sum(np.array(total_loss))
-        
-        if verbose: 
-            print("Model loss: ", loss)
-
-        return loss
-    
-    def gradient(self):
-        """ 
-        Calculates the gradient for the Logistic 
-        Regression model 
-        """
-        grad = np.zeros((self.m,)) # initialize gradient
-        
-        # calcualte gradient of each example 
-        # and add together
-        for i in range(self.n): 
-            x_i = self.X[i] 
-            y_i = self.y[i] 
-            wTx = float(np.matmul(T(self.w),x_i)) # w^T x
-            sigm_wTx = self.sigmoid(wTx)
-            grad += x_i*(y_i - sigm_wTx) # add to previous grad
-            
-        return grad.reshape((len(grad),1))
-    
-    def train(self, alpha=0.002, threshold=0.001, epochs=100, auto_alpha=0.99, verbose=True): 
-        
-        # initialize error
-        prev_loss = self.cross_entropy_loss()
-        initial_loss = prev_loss
-        
-        if verbose: 
-            for k in tqdm(range(epochs), desc="\nTraining...") : 
-                
-                grad = alpha*self.gradient() # calculate gradient   
-                temp = np.add(self.w, grad) # get weights update
-                self.w = temp # update weights
-                loss = self.cross_entropy_loss() # calculate current error
-                
-                print("\nEpoch {}".format(k+1))
-                print("Cross-entropy loss: %.2f" % (loss) )
-                            
-                if abs(loss-prev_loss) < threshold: 
-                    break
-                
-                prev_loss = loss
-                alpha = auto_alpha*alpha
-                
-        else: 
-            for k in range(epochs): 
-                
-                grad = alpha*self.gradient() # calculate gradient   
-                temp = np.add(self.w, grad) # get weights update
-                self.w = temp # update weights
-                loss = self.cross_entropy_loss() # calculate current error
-                            
-                if abs(loss-prev_loss) < threshold: 
-                    break
-                
-                prev_loss = loss
-                alpha = auto_alpha*alpha
-            
-            
-        print("---Terminated---")  
-        
-        final_loss = prev_loss
-        print("Initial loss: {}".format(initial_loss)) 
-        print("Final loss: {}".format(final_loss))
-        
-        return final_loss
-        
-    
-    def fit(self, X,y,alpha=0.02, threshold=0.001, epochs=100, auto_alpha=0.99, verbose=False): 
-        """
-        Initializes the model with the input parameters 
-        and trains
-        """
-        self.__init__(X,y) # Initialize with input 
-        self.train(alpha, threshold, epochs, auto_alpha, verbose)
-        
-    
-
-# TESTS :
         
 # subset for test
 X_new = X_redwine[0:10,:] # matrix
@@ -410,10 +199,9 @@ x_new.shape
 # Way # 1
         
 logreg = LogisticRegression(X_redwine, y_redwine) # initialize 
-logreg.sigmoid(4) # calculate sigmoid 
 logreg.cross_entropy_loss(verbose=False) # calculate loss 
 logreg.gradient() # get gradient
-logreg.train(alpha=0.002, threshold=0.001, epochs=100, auto_alpha=0.99, verbose=False) # run gradient descent and train the model 
+final_loss = logreg.train(alpha=0.002, threshold=0.001, epochs=100, auto_alpha=0.99, verbose=False) # run gradient descent and train the model 
 logreg.predict_probabilities(X_new) # predict vector of probablities
 y_pred = logreg.predict(X_new) # predict classifications
 print("y_pred ", y_pred)
@@ -434,16 +222,19 @@ print("y_pred ", y_pred)
 print("y_new ", list(y_new))
 print("toy accuracy: {}%".format( (y_pred == y_new).sum() / len(y_pred)*100 ))
 
+# We can plot the training loss! 
+logreg.plot_training_loss()
+
 
 # Parameter search algorithm: 
-alphas = alpha = [0.001, 0.002, 0.003, 0.004]
+alphas = [0.002, 0.01, 0.05, 0.1]
 
 losses = {}
 accuracies = {}
 # for each option for alpha 
 for alpha in alphas: 
     # get the name of the alpha used
-    model_name = "logreg_alpha{}: \n".format(alpha)
+    model_name = "logreg_alpha_{}: \n".format(alpha)
     # instantiate the model
     model = LogisticRegression() 
     # fit and train the model with the appropriate X and y
@@ -459,3 +250,74 @@ for alpha in alphas:
     # store in accuracies dictionary 
     accuracies[model_name] = acc
     print("\n")
+    
+
+# ********************************************************************************
+    
+### 4. Testing LDA ### 
+    
+# PLEASE INPUT LDA TESTS IN HERE ONCE LDA IS COMPLETE  
+    
+# ********************************************************************************
+    
+### 5. Cross validation, parameter search, evaluation and performance ### 
+    
+
+X = np.random.rand(10,3) 
+X = pd.DataFrame(X)  
+    
+def cross_split(X,folds=5): 
+    
+    # for each split"
+    for k in range(folds): 
+        
+        # display split number
+        print("\nSplit {}\n".format(k+1))
+        
+        split = X.shape[0] / folds # obtain splitting index
+        upper = int(split*(k+1)) # obtain upper index
+        lower = max(int(upper - split),0) # obtain lower index 
+        
+        print("lower = {}".format(lower))
+        print("upper = {}".format(upper)) 
+        
+        X_lower = X.iloc[:lower,:]  # obtain lower train set
+        X_val = X.iloc[lower:upper,:]  # obtain validation set 
+        X_upper = X.iloc[upper:,:]  # obtain upper train set
+        
+        # stack lower train and upper train together
+        X_train = X_lower.append(X_upper) 
+        
+        print("X_train: ", X_train) 
+        print("\nX_val :{} \n".format(X_val))
+        
+
+
+def kfold_CV_search(algorithm, X, y, k = 5, params={}, random_state=42): 
+    """ 
+    Performs a k-fold Cross-validation search testing the given parameters, 
+    which have to be input as a dictionary, where the keys are the 
+    values of the parameters to test and the values are lists which include 
+    a range of parameters to test. 
+    
+    params = {"alpha" :[0.001, 0.002, 0.003], 
+              "threshold" : [0.001, 0.01, 0.1], 
+              "auto_alpha" : [0.99, 0.95, 0.90]
+               } 
+
+    params: 
+        @ algorithm: an instance of a classification algorithm (log reg) 
+        @ X: input training features data
+        @ y: input targets data  
+        @ k: number of folds to perform 
+        @ params: dictionary of parameters to test
+        @ random_state: seed for the random 
+    """
+    
+    random_state = random_state 
+    X = pd.DataFrame(X) 
+    X = X.iloc[np.random.permutation(len(X))] # randomly shuffle the data 
+   
+
+    return X 
+    
